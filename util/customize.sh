3fd0fe28c4dfe2317a7115b7186bd5b41f4564ce
Techflash <72118300+techflashYT@users.noreply.github.com>
2022-11-16 16:11:35 -0700

    emphasize even more how bad of an idea option 2 is

--- util/customize.sh
+++ util/customize.sh
@@ -6,7 +6,7 @@ leaveCustomize() {
 }
 customize() {
 	warning "\x1b[31mHey, are you sure you want to customize the install?\x1b[0m"
-	warning "The default settings are just fine, and changing any of them will likely make it not work correctly with Techflash OS."
+	warning "The default settings are just fine, and changing any of them \x1b[33mwill likely make it not work correctly\x1b[0m with Techflash OS."
 	warning "If this isn't the case and the default settings somehow don't work, please file a bug report at the link below."
 	warning "\x1b[1;4;36mhttps://github.com/techflashYT/x86_64-elf-gcc-cross-compiler-builder\x1b[0m"
 	warning "If you are \x1b[33mABSOLUTELY\x1b[0m positive that you need to be here, say \x1b[31myes\x1b[0m, otherwise, say \x1b[32mno\x1b[0m"
