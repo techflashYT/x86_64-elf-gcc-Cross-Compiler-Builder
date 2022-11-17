ac35713fa6e4cc5a8067f5edbb09ffda4c9272c6
Techflash <72118300+techflashYT@users.noreply.github.com>
2022-11-16 19:29:21 -0700

    Get version from downloaded source, and display it

--- util/download.sh
+++ util/download.sh
@@ -7,7 +7,7 @@ download() {
 	progress "Downloading the Binutils source"
 	if [ $lsRet -eq 0 ]; then
 		while true; do
-			note "Binutils source already cloned, would you like to redownload it? (yes/no) "
+			printf "\x1b[1;36m===> NOTE:\x1b[0m Binutils source already cloned, would you like to redownload it? (yes/no) "
 			read yesNo
 			case $yesNo in
 				yes) rm -rf tmp/binutils; doDownload=true; break;;
@@ -21,4 +21,7 @@ download() {
 	if [ $doDownload = true ]; then
 		git clone git://sourceware.org/git/binutils-gdb.git --depth=1 tmp/binutils
 	fi
+	binutilsVer=$(printf "$(grep "PACKAGE_VERSION='" tmp/binutils/bfd/configure)" | sed 's/PACKAGE_VERSION='\''//g' | sed 's/'\''//g') # Get the version of the downloaded source
+	progress "Binutils \x1b[1;36mv$binutilsVer\x1b[0m downloaded."
+	progress "Downloading the GCC source"
 }
